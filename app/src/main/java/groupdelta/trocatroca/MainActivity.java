c20b212f2cda111a4e0cb75e2ee487fdac5ad63b
agojunior <junior092006@hotmail.com>
2018-08-25 17:10:56 -0300

    Created main activity and register activity. (configure behavior of textview,edittest and buttons) connect login screen to register screen. created basic register screen.

---
+++ app/src/main/java/groupdelta/trocatroca/MainActivity.java
@@ -0,0 +1,42 @@
+package groupdelta.trocatroca;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.support.annotation.Nullable;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    /* E-mail edit text */
+    private EditText mEmailEditText;
+
+    /* Password edit text */
+    private EditText mPasswordEditText;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /*Obtaining the references to the views from the XML.*/
+        mEmailEditText = findViewById(R.id.edtEmail);
+        mPasswordEditText = findViewById(R.id.edSenha);
+    }
+
+    public void onRegisterButtonClicked(View view) {
+        Context context = this;
+
+        Class destinationClass = CadastroActivity.class;
+        Intent intentToStartRegisterActivity = new Intent(context, destinationClass);
+
+        startActivity(intentToStartRegisterActivity);
+    }
+
+    public void onLoginButtonClicked(View view) {
+    }
+}
