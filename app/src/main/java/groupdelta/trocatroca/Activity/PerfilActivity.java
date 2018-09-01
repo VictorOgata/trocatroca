d38f378da36b431ac210d3806495f7dacf27a123
Victor <victoroliveiraogata@hotmail.com>
2018-09-01 18:51:35 -0300

    Profile Changes

--- app/src/main/java/groupdelta/trocatroca/Activity/PerfilActivity.java
+++ app/src/main/java/groupdelta/trocatroca/Activity/PerfilActivity.java
@@ -3,10 +3,21 @@ package groupdelta.trocatroca.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
+import com.google.firebase.auth.FirebaseAuth;
+
+import groupdelta.trocatroca.Entities.Usuario;
 import groupdelta.trocatroca.R;
 
 public class PerfilActivity extends AppCompatActivity{
+
+    private Usuario user;
+    private EditText Email;
+    private EditText Password;
+
+    private FirebaseAuth aut;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -14,7 +25,8 @@ public class PerfilActivity extends AppCompatActivity{
         setContentView(R.layout.activity_perfil);
     }
 
-    public void onModifyUserNameButtonClicked
+    public void onModifyUserNameButtonClicked(View view) {
 
     }
 }
+
