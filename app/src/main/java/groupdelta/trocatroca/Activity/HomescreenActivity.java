7991c802f23418d3d5273825bee35ab1c2df81ea
Victor <victoroliveiraogata@hotmail.com>
2018-09-08 17:49:05 -0300

    Homescreen Modifications

--- app/src/main/java/groupdelta/trocatroca/Activity/HomescreenActivity.java
+++ app/src/main/java/groupdelta/trocatroca/Activity/HomescreenActivity.java
@@ -7,12 +7,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+
+import groupdelta.trocatroca.DataAccessObject.Conexao;
 import groupdelta.trocatroca.R;
 
 public class HomescreenActivity extends AppCompatActivity {
     private Button btnPerfil;
     private Button btnLogout;
     private Button btnNewItem;
+    private FirebaseAuth auth;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +26,6 @@ public class HomescreenActivity extends AppCompatActivity {
         btnPerfil = findViewById(R.id.btnPerfil);
         btnLogout = findViewById(R.id.btnLogout);
         btnNewItem = findViewById(R.id.btnNewItem);
-
     }
     public void onPerfilButtonClicked(View view) {
         Intent i = new Intent(HomescreenActivity.this, PerfilActivity.class);
@@ -30,7 +33,9 @@ public class HomescreenActivity extends AppCompatActivity {
     }
     public void onLogoutButtonClicked(View view) {
         Intent i = new Intent(HomescreenActivity.this, MainActivity.class);
+        Conexao.logOut();
         startActivity(i);
+
     }
     public void onNovaofertaButtonClicked(View view) {
         Intent i = new Intent(HomescreenActivity.this, PerfilActivity.class);
