5bdd9a09677ee9a9c1a3e0dc48a58f43ac33fc81
Antonio Gomes de Oliveira Junior <junior092006@hotmail.com>
2018-09-09 21:26:23 -0300

    changes to new offer screen

--- app/src/main/java/groupdelta/trocatroca/Activity/NovoAnuncioActivity.java
+++ app/src/main/java/groupdelta/trocatroca/Activity/NovoAnuncioActivity.java
@@ -1,24 +1,29 @@
 package groupdelta.trocatroca.Activity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import groupdelta.trocatroca.R;
+
+import static android.widget.Toast.LENGTH_LONG;
 
 public class NovoAnuncioActivity extends AppCompatActivity {
     private String[] itens = new String[] {"Game of Thrones", "Age of Empires", "Prototype", "God of War", "God of War2", "god of war", "game of Throne2"};
 
     /* Nickname edit text*/
-    private EditText mNickEditText;
+    private EditText mItemEditText;
     /* E-mail edit text */
-    private EditText mEmailEditText;
+    private EditText mDesejadosEditText;
     /* Password edit text */
-    private EditText mPasswordEditText;
+    private EditText mDescricaoEditText;
     /* Confirm Password edit text */
-    private EditText mConfPassEditText;
+    private EditText mCondicoesEditText;
 
 
 
@@ -29,12 +34,21 @@ public class NovoAnuncioActivity extends AppCompatActivity {
 
 
         /*Obtaining the references to the views from the XML.*/
-        mNickEditText = findViewById(R.id.edtApelidoCadastro);
-        mEmailEditText = findViewById(R.id.edtEmailCadastro);
-        Intent intentMA = getIntent();
-        String emailReceived = (String) intentMA.getStringExtra("email");
-        mEmailEditText.setText(emailReceived);
-        mPasswordEditText = findViewById(R.id.edtSenhaCadastro);
-        mConfPassEditText = findViewById(R.id.edtConfSenha);
+        mItemEditText = findViewById(R.id.edtItemCadastro);
+        mDesejadosEditText = findViewById(R.id.edtDesejadosCadastro);
+        mDescricaoEditText = findViewById(R.id.edtDescricaoCadastro);
+        mCondicoesEditText = findViewById(R.id.edtCondicoes);
+    }
+
+    public void onOKNewButtonClicked(View view) {
+        Context context=this;
+        /*Checking non informed parameters*/
+        if(!mItemEditText.getText().toString().isEmpty() && !mDesejadosEditText.getText().toString().isEmpty()&&
+                !mDescricaoEditText.getText().toString().isEmpty() && !mCondicoesEditText.getText().toString().isEmpty()){
+
+            Toast.makeText(context,(String)"Inserindo Anuncio.", LENGTH_LONG).show();
+        }else{
+            Toast.makeText(context,(String)"Campos não preenchidos.", LENGTH_LONG).show();
+        }
     }
 }
