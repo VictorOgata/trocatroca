ec0593f96061236eece9abdecc9e0bdefca0dbe7
Antonio Gomes de Oliveira Junior <junior092006@hotmail.com>
2018-09-09 21:54:15 -0300

    select if game or book for new item

--- app/src/main/java/groupdelta/trocatroca/Activity/NovoAnuncioActivity.java
+++ app/src/main/java/groupdelta/trocatroca/Activity/NovoAnuncioActivity.java
@@ -6,7 +6,9 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import groupdelta.trocatroca.R;
@@ -14,6 +16,7 @@ import groupdelta.trocatroca.R;
 import static android.widget.Toast.LENGTH_LONG;
 
 public class NovoAnuncioActivity extends AppCompatActivity {
+    public static final String[] itemType = {"Jogo ou Livro?","Livro","Jogo"};
     private String[] itens = new String[] {"Game of Thrones", "Age of Empires", "Prototype", "God of War", "God of War2", "god of war", "game of Throne2"};
 
     /* Nickname edit text*/
@@ -23,7 +26,7 @@ public class NovoAnuncioActivity extends AppCompatActivity {
     /* Password edit text */
     private EditText mDescricaoEditText;
     /* Confirm Password edit text */
-    private EditText mCondicoesEditText;
+    private Spinner mTypeSpinner;
 
 
 
@@ -37,14 +40,21 @@ public class NovoAnuncioActivity extends AppCompatActivity {
         mItemEditText = findViewById(R.id.edtItemCadastro);
         mDesejadosEditText = findViewById(R.id.edtDesejadosCadastro);
         mDescricaoEditText = findViewById(R.id.edtDescricaoCadastro);
-        mCondicoesEditText = findViewById(R.id.edtCondicoes);
+        mTypeSpinner = findViewById(R.id.edtCondicoes);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(NovoAnuncioActivity.this,
+                android.R.layout.simple_spinner_item,itemType);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mTypeSpinner.setAdapter(adapter);
     }
 
     public void onOKNewButtonClicked(View view) {
         Context context=this;
         /*Checking non informed parameters*/
         if(!mItemEditText.getText().toString().isEmpty() && !mDesejadosEditText.getText().toString().isEmpty()&&
-                !mDescricaoEditText.getText().toString().isEmpty() && !mCondicoesEditText.getText().toString().isEmpty()){
+                !mDescricaoEditText.getText().toString().isEmpty() && !mTypeSpinner.getSelectedIt
+
+        em().toString().equals(itemType[0])){
 
             Toast.makeText(context,(String)"Inserindo Anuncio.", LENGTH_LONG).show();
         }else{
