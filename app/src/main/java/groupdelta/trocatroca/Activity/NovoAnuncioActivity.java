ae4b66400d1c51f868a9e8c2c4277653380c9333
Antonio Gomes de Oliveira Junior <junior092006@hotmail.com>
2018-09-10 01:07:25 -0300

    Stated sending items to database, still need to be changed. This is only a prototype.

--- app/src/main/java/groupdelta/trocatroca/Activity/NovoAnuncioActivity.java
+++ app/src/main/java/groupdelta/trocatroca/Activity/NovoAnuncioActivity.java
@@ -1,8 +1,10 @@
 package groupdelta.trocatroca.Activity;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -11,6 +13,16 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseUser;
+
+import groupdelta.trocatroca.DataAccessObject.Conexao;
+import groupdelta.trocatroca.Entities.Anuncio;
+import groupdelta.trocatroca.Entities.Usuario;
 import groupdelta.trocatroca.R;
 
 import static android.widget.Toast.LENGTH_LONG;
@@ -19,7 +31,7 @@ public class NovoAnuncioActivity extends AppCompatActivity {
     public static final String[] itemType = {"Jogo ou Livro?","Livro","Jogo"};
     private String[] itens = new String[] {"Game of Thrones", "Age of Empires", "Prototype", "God of War", "God of War2", "god of war", "game of Throne2"};
 
-    /* Nickname edit text*/
+    /* Itemname edit text*/
     private EditText mItemEditText;
     /* E-mail edit text */
     private EditText mDesejadosEditText;
@@ -27,6 +39,8 @@ public class NovoAnuncioActivity extends AppCompatActivity {
     private EditText mDescricaoEditText;
     /* Confirm Password edit text */
     private Spinner mTypeSpinner;
+    private FirebaseAuth autentication;
+    Anuncio ad;
 
 
 
@@ -55,6 +69,17 @@ public class NovoAnuncioActivity extends AppCompatActivity {
                 !mDescricaoEditText.getText().toString().isEmpty() && !mTypeSpinner.getSelectedItem().toString().equals(itemType[0])){
 
             Toast.makeText(context,(String)"Inserindo Anuncio.", LENGTH_LONG).show();
+            ad= new Anuncio();
+            FirebaseUser firebaseUser = Conexao.getFirebaseAuth().getCurrentUser();
+            ad.setHost(firebaseUser.getUid());
+            /* Pulling information from screen through references*/
+            ad.setItem(mItemEditText.getText().toString());
+            ad.setWishList(mDesejadosEditText.getText().toString().replace(" ", "").toUpperCase());
+            ad.setDescription(mDescricaoEditText.getText().toString());
+            ad.setType(mTypeSpinner.getSelectedItem().toString());
+            ad.saveNewAd(context);
+            Intent i = new Intent( NovoAnuncioActivity.this, HomescreenActivity.class);
+            startActivity(i);
         }else{
             Toast.makeText(context,(String)"Campos não preenchidos.", LENGTH_LONG).show();
         }
---
+++ app/src/main/java/groupdelta/trocatroca/Entities/Anuncio.java
@@ -0,0 +1,87 @@
+package groupdelta.trocatroca.Entities;
+
+import android.content.Context;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+
+import groupdelta.trocatroca.DataAccessObject.Conexao;
+
+import static android.widget.Toast.LENGTH_LONG;
+
+public class Anuncio {
+
+    //User itemname
+    private String item;
+    //User description
+    private String description;
+    //User contact info
+    private String WishList;
+    //User state (Country area)
+    private String state;
+    //User city
+    private String city;
+
+    private String host;
+    private String type;
+
+    public Anuncio() {
+    }
+
+    public void saveNewAd(Context context){
+        DatabaseReference referenciaFirebase = Conexao.getFirebaseReference();
+        FirebaseUser firebaseUser = Conexao.getFirebaseAuth().getCurrentUser();
+        if (firebaseUser != null) {
+            if(!(this.item.isEmpty() || this.description.isEmpty() || this.WishList.isEmpty()))
+                referenciaFirebase.child("Anuncios").child(this.host).setValue(this);
+            else
+                Toast.makeText(context, (String) "Empty User Parameters.", LENGTH_LONG).show();
+        }
+        else{
+            Toast.makeText(context, (String) "Usuario nao autenticado.", LENGTH_LONG).show();
+        }
+    }
+
+    public String getItem() {
+        return item;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public String getCity() {
+        return city;
+    }
+    public String getState() {
+        return state;
+    }
+    public String getWishList() { return WishList; }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+    public String getType() { return type; }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+    public void setWishList(String WishList) {
+        this.WishList = WishList;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public void setState(String state) {
+        this.state = state;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public String getHost() { return host; }
+}
