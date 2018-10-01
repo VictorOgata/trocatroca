abd3dceacf5cf72c5c6a668045e94e2ab0ca581f
Antonio Gomes de Oliveira Junior <junior092006@hotmail.com>
2018-09-30 23:24:08 -0300

    fixed small problens in showing user ads, now working

--- app/src/main/java/groupdelta/trocatroca/Activity/MeusAnunciosActivity.java
+++ app/src/main/java/groupdelta/trocatroca/Activity/MeusAnunciosActivity.java
@@ -3,8 +3,11 @@ package groupdelta.trocatroca.Activity;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -22,8 +25,9 @@ import groupdelta.trocatroca.Entities.Advertisement;
 import groupdelta.trocatroca.R;
 
 public class MeusAnunciosActivity  extends  AppCompatActivity {
-    private DatabaseReference myRef;
+    private EditText editPalavra;
     private ListView Busca;
+    private DatabaseReference myRef;
     private FirebaseDatabase firebaseDatabase;
     private List<Advertisement> listAdvertisementClasses = new ArrayList<Advertisement>();
     private List<String> listAnuncioNames = new ArrayList<String>();
@@ -32,10 +36,36 @@ public class MeusAnunciosActivity  extends  AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meusanuncios);
-        Busca = findViewById(R.id.ListSearch);
 
-        myRef = FirebaseDatabase.getInstance().getReference("Usuarios");
+        editPalavra= (EditText) findViewById(R.id.TextSearch);
+        Busca=(ListView) findViewById(R.id.ListSearch);
+
+        myRef = FirebaseDatabase.getInstance().getReference("Anuncios");
         AdapterView<?> parent;
+        eventEdit();
+
+
+    }
+
+    private void eventEdit(){
+        editPalavra.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String word = editPalavra.getText().toString().trim();
+                ListaAnuncios();
+
+            }
+        });
 
     }
 
