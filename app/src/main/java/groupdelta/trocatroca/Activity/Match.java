3b5c35787d1efa3b0dcd48270649f17fbfd62fd8
Unknown <victoroliveiraogata@hotmail.com>
2018-10-16 23:54:10 -0300

    init Match interface

---
+++ app/src/main/java/groupdelta/trocatroca/Activity/Match.java
@@ -0,0 +1,16 @@
+package groupdelta.trocatroca.Activity;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import groupdelta.trocatroca.R;
+
+public class Match extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_match);
+    }
+
+}
