e6ae78fecdd5e9ce803299f73ba69797add29286
Unknown <victoroliveiraogata@hotmail.com>
2018-10-19 23:48:47 -0300

    Minor changes

---
+++ app/src/main/java/groupdelta/trocatroca/Activity/Chat.java
@@ -0,0 +1,17 @@
+package groupdelta.trocatroca.Activity;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+
+import groupdelta.trocatroca.R;
+
+public class Chat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+
+}
