4bafc1a456d08d8a0b5321469112d80b7603dcdb
Gaspar <r.gasparafonso@gmail.com>
2018-10-21 18:37:53 -0300

    Request and Trade screens created
    activity_minha_solicitacoes.xml and actyvity_trade,xml respectively

---
+++ app/src/main/java/groupdelta/trocatroca/Activity/MinhasSolicitacoesActivity.java
@@ -0,0 +1,15 @@
+package groupdelta.trocatroca.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import groupdelta.trocatroca.R;
+
+public class MinhasSolicitacoesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_minhas_solicitacoes);
+    }
+}
