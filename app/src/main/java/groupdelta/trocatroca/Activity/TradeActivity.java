27dbfb781d57a2b4364d9941f7f8bd219f79f8be
Unknown <victoroliveiraogata@hotmail.com>
2018-11-10 17:51:00 -0200

    minor updates(chat)

--- app/src/main/java/groupdelta/trocatroca/Activity/TradeActivity.java
+++ app/src/main/java/groupdelta/trocatroca/Activity/TradeActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -111,7 +112,15 @@ public class TradeActivity extends AppCompatActivity {
         });
 
         //BUTTON LISTENERS DECLARATIONS
+        btnTradeChat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(TradeActivity.this, ChatAcivity.class);
+                startActivity(i);
+            }
+        });
     }
+
 
     //QUERY DATA RETURN
     private void saveTradeData(DataSnapshot dataSnapshot) {
