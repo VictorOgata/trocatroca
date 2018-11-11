9c81b29dd3086528c8ecec75794f06ea40e9e6aa
Unknown <victoroliveiraogata@hotmail.com>
2018-11-11 00:03:26 -0200

    Working on chat

--- app/src/main/java/groupdelta/trocatroca/Entities/Message.java
+++ app/src/main/java/groupdelta/trocatroca/Entities/Message.java
@@ -2,19 +2,31 @@ package groupdelta.trocatroca.Entities;
 
 public class Message {
     private String textMessage;
-    private String messageTime;
+    private long messageTime;
+    private String Usermessage;
+    private String userID1,userID2;
 
     public Message() {
     }
-
+    public String getUserID1() {
+        return userID1;
+    }
+    public String getUserID2() {
+        return userID2;
+    }
+    public String getUser1Message() {
+        return Usermessage;
+    }
     public String getTextMessage() {
         return textMessage;
     }
-    public String getTime() {
+    public long getTime() {
         return messageTime;
     }
-
-    public void setTime(String messageTime) {
+    public void setT(long messageTime) {
+        this.messageTime = messageTime;
+    }
+    public void setTime(long messageTime) {
         this.messageTime = messageTime;
     }
     public void setTextMessage(String textMessage) {
