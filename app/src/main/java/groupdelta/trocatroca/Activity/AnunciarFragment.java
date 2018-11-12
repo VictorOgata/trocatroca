677551c1696387dfd56d9f781a1d1489ae6ce931
Antonio Gomes de Oliveira Junior <junior092006@hotmail.com>
2018-11-12 00:34:39 -0200

    small text changes (meus anuncios -> procurar matches), now that the cards are working, the 'meus anuncios' screen is redundant and can be replaced with the screen for finding wishlist matches.

--- app/src/main/java/groupdelta/trocatroca/Activity/AnunciarFragment.java
+++ app/src/main/java/groupdelta/trocatroca/Activity/AnunciarFragment.java
@@ -5,16 +5,12 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -62,7 +58,7 @@ public class AnunciarFragment extends Fragment {
         btnMyAd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(AnunciarFragment.super.getContext(),MeusAnunciosActivity.class);
+                Intent i = new Intent(AnunciarFragment.super.getContext(),FindMatchWishlist.class);
                 startActivity(i);
             }
         });
